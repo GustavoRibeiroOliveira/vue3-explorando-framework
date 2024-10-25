eced3e1cc7a1f85fd5e75c1f14a75e8ec6a7f349
Gustavo Ribeiro de Oliveira <gustavo.r@inatel.com>
2024-10-25 11:01:20 -0300

    Componentizando o formulario e o temporizador e finalizando as funcções do temporizador

--- alura-tracker/src/App.vue
+++ alura-tracker/src/App.vue
@@ -4,7 +4,7 @@
       <BarraLateral />
     </div>
     <div class="column is-three-quarter">
-      <FormularioHome />
+      <Formulario />
     </div>
   </main>
 </template>
@@ -12,11 +12,11 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import BarraLateral from './components/BarraLateral.vue';
-import FormularioHome from './components/FormularioHome.vue';
+import Formulario from './components/Formulario.vue';
 
 export default defineComponent({
   name: 'App',
-  components: { BarraLateral, FormularioHome }
+  components: { BarraLateral, Formulario }
 });
 </script>
 
